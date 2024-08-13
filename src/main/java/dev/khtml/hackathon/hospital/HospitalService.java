7fdbf9cfeec3015b1d8fd709378b26d248512f71
Kang-bh <kabyho3513@gmail.com>
2024-08-14 02:40:36 +0900

    etc: add log

--- src/main/java/dev/khtml/hackathon/hospital/HospitalService.java
+++ src/main/java/dev/khtml/hackathon/hospital/HospitalService.java
@@ -25,9 +25,13 @@ public class HospitalService {
 		final double currentLatitude = request.latitude();
 		final double currentLongitude = request.longitude();
 
+		long startTime = System.currentTimeMillis(); // Start time logging
 		// 프롬프트로 reason 리스트 받아오면서, 진료과목도 함께 받아오기
 		String result = promptService.getResult(request.prompt());
 		List<ReasonResponse> parsedReason = JsonMapper.parseJsonAndMapToReasonResponse(result);
+		long endTime = System.currentTimeMillis(); // Start time logging
+		log.info(">>>>> getAppropriateHospitals executed in {} ms", (endTime - startTime));
+
 
 		try {
 			// 해당 진료과목을 가진 병원 중, 가장 가까운 병원 찾아서 반환
@@ -49,7 +53,12 @@ public class HospitalService {
 			// 같은 진료과목의 서로다른 병원들
 			double minDistance = Double.MAX_VALUE;
 			Hospital nearestHospital = null;
+			long startTime2 = System.currentTimeMillis();
 			List<MedicalDepartment> byDepartment = medicalDepartmentRepository.findByDepartment(departmentEnum);
+			long endTime2 = System.currentTimeMillis();
+			log.info(">>>>> findByDepartment executed in {} ms", (endTime2 - startTime2));
+
+			long startTime3 = System.currentTimeMillis();
 			for (MedicalDepartment dep : byDepartment) {
 				Hospital hospital = dep.getHospital();
 				double distance = LatLonCalculator.getDistance(
@@ -63,6 +72,9 @@ public class HospitalService {
 					nearestHospital = hospital;
 				}
 			}
+			long endTime3 = System.currentTimeMillis();
+			log.info(">>>>> calculate executed in {} ms", (endTime3 - startTime3));
+
 			// 그 중, 가장 가까운 병원
 			if (nearestHospital != null) {
 				reason.updateHospital(nearestHospital, minDistance);
